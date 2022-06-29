61b7c8a34a8f0479f863bd4d84f0e8a159464151
SANAYEE <dudtkd0219@gmail.com>
2022-06-29 17:40:48 +0900

    리팩토링

--- src/main/java/webserver/HttpResponse.java
+++ src/main/java/webserver/HttpResponse.java
@@ -1,8 +1,11 @@
 package webserver;
 
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.util.HashMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,17 +13,73 @@ import org.slf4j.LoggerFactory;
 public class HttpResponse {
 	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 	
-	DataOutputStream dos;
-	byte[] body;
+	private DataOutputStream dos;
+	private HashMap<String, String> responseHeaderMap;
+	private static final String message200 = "HTTP/1.1 200 OK \\r\\n";
+	private static final String message302 = "HTTP/1.1 302 OK \\r\\n";
 	
-	public HttpResponse(OutputStream out, byte[] body) {
+	
+	public HttpResponse(OutputStream out) {
 		this.dos = new DataOutputStream(out);
-		this.body = body;
+	}
+	
+	
+	public byte[] getHtml(String url) {
+		byte[] body = null;
+		try {
+			body = Files.readAllBytes(new File("./webapp" + url).toPath());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return body;
+	}
+	
+	public void addHeader(String key, String value) {
+		responseHeaderMap.put(key, value);
+	}
+	
+	public void forward(String url) {
+		if(url.contains("css")) {
+			
+		}
+			
+	}
+	
+	public void forwardBody() {
+		
+		
+	}
+	
+	public void response200Header() {
+		try {
+			dos.writeBytes(message200);
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void sendRedirect(String location) {
+		try {
+			dos.writeBytes(message302);
+			dos.writeBytes("Location: "+location);
+			dos.writeBytes("\r\n");
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	public void processHeaders() {
+		
+		
 	}
 	
 	
 	
-	private void responseBody(DataOutputStream dos, byte[] body) {
+	private void responseBody(byte[] body) {
         try {
             dos.write(body, 0, body.length);
             dos.flush();
